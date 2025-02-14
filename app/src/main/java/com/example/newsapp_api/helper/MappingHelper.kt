7bb4eea99bf956c6a45c5f52fcc9e9f4cf4bdca8
Zaki Burnama <zaki.burnama1017@gmail.com>
2025-02-14 11:35:36 +0700

    make mapping helper

---
+++ app/src/main/java/com/example/newsapp_api/helper/MappingHelper.kt
@@ -0,0 +1,27 @@
+package com.example.newsapp_api.helper
+
+import android.database.Cursor
+import com.example.newsapp_api.Items
+import com.example.newsapp_api.db.DatabaseContract
+
+object MappingHelper {
+
+    fun mapCursorToArrayList(notesCursor: Cursor?): ArrayList<Items> {
+        val newsList = ArrayList<Items>()
+        notesCursor?.apply {
+            while (moveToNext()) {
+                val id = getInt(getColumnIndexOrThrow(DatabaseContract.NewsColumns._ID))
+                val name = getString(getColumnIndexOrThrow(DatabaseContract.NewsColumns.NAME))
+                val author = getString(getColumnIndexOrThrow(DatabaseContract.NewsColumns.AUTHOR))
+                val title = getString(getColumnIndexOrThrow(DatabaseContract.NewsColumns.TITLE))
+                val description = getString(getColumnIndexOrThrow(DatabaseContract.NewsColumns.DESCRIPTION))
+                val url = getString(getColumnIndexOrThrow(DatabaseContract.NewsColumns.URL))
+                val urlToImg = getString(getColumnIndexOrThrow(DatabaseContract.NewsColumns.URL_TO_IMG))
+                val date = getString(getColumnIndexOrThrow(DatabaseContract.NewsColumns.DATE))
+//                val isFavorite = getInt(getColumnIndexOrThrow(DatabaseContract.NewsColumns.FAVORITE))
+                newsList.add(Items(name, author, title, description, url, urlToImg,  date, id, true))
+            }
+        }
+        return newsList
+    }
+}
